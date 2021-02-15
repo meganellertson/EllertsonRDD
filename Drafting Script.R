048e6a5bc91489313f5ca0820c1e863e01ca2ccb
meganellertson <78630116+meganellertson@users.noreply.github.com>
2021-02-15 10:33:14 -0800

    Updated Question 6 and added to drafting script more work with McCrary Density

---
+++ Drafting Script.R
@@ -0,0 +1,16 @@
+McCrary Density 
+
+libary(rddensity)
+rdbwdensity(
+  bac1,
+  c = 0.8 & 0.15,
+  p = 2,
+  fitselect = "",
+  kernel = "",
+  vce = "",
+  masPoint = TRUE,
+  regularize = TRUE,
+  nLocalMin = NULL,
+  nUniqueMin = NULL
+  
+)
